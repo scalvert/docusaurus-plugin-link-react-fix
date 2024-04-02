e2e5ec757e69ebcfb0405e69cac92e9c60034820
Steve Calvert <scalvert@linkedin.com>
2024-04-02 08:18:50 -0700

    Fixing path to resolve relative to root project

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const path = require("path");
+const findUp = require("find-up");
 
 module.exports = function (context, options) {
   return {
     name: "link-react-fix",
     configureWebpack(config, isServer, utils) {
-      const { getCacheLoader } = utils;
+      let parentProjectDir = findUp.sync("docusaurus.config.json", {
+        cwd: __dirname,
+      });
 
       return {
         resolve: {
           alias: {
-            react: path.resolve(__dirname, "./node_modules/react"),
-            "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
+            react: path.resolve(parentProjectDir, "./node_modules/react"),
+            "react-dom": path.resolve(
+              parentProjectDir,
+              ".node_modules/react-dom",
+            ),
           },
         },
       };
